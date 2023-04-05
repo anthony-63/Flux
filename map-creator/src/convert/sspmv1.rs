39b6b4fbc73fea2fc470d35a2b7af52d62c0d1df
aixeria <44528100+pozm@users.noreply.github.com>
2023-04-05 09:43:52 +0100

    sspm parser & mapdownloader

--- map-creator/src/convert/sspmv1.rs
+++ map-creator/src/convert/sspmv1.rs
@@ -1,24 +1,136 @@
-use std::io::{BufReader, Cursor, BufRead, Read};
+use std::io::{BufReader, Cursor, BufRead, Read, Seek, SeekFrom};
 
-pub fn convert_sspm1(data:Vec<u8>) -> Vec<u8> {
-    let ret = Vec::new();
-    let mut r =BufReader::new(Cursor::new(data));
-    let mut _dnc = String::new();
-    r.read_line(&mut _dnc);
-    r.read_line(&mut _dnc);
-    r.read_line(&mut _dnc);
-    print!("{_dnc}");
-    let mut note_count = [0;4];
-    r.read_exact(&mut [0;4]);
-    r.read_exact(&mut note_count);
-    let mut diff = [0;1];
-    r.read_exact(&mut diff);
-    r.read_exact(&mut [0;1]);
-    let mut img_type = [0;1];
-    r.read_exact(&mut img_type);
-    // r.read
+use binrw::BinReaderExt;
 
-    println!("{note_count:?} {diff:?} {img_type:?}");
+use crate::FluxMap;
 
-    ret
+pub struct SSPM1 {
+    music_data : Vec<u8>,
+    map_data : Vec<SSPM1Note>,
+    id : String,
+    name : String,
+    creator : String,
+}
+pub struct SSPM1NoteF {
+    time : u32,
+    x : f32,
+    y : f32,
+}
+pub struct SSPM1Note8 {
+    time : u32,
+    x : f32,
+    y : f32,
+}
+pub enum SSPM1Note {
+    Float(SSPM1NoteF),
+    Int(SSPM1Note8),
+}
+impl SSPM1Note {
+    pub fn time(&self) -> u32 {
+        match self {
+            SSPM1Note::Float(x) => x.time,
+            SSPM1Note::Int(x) => x.time,
+        }
+    }
+}
+
+impl TryFrom<Vec<u8>> for SSPM1 {
+    type Error = ();
+    fn try_from(data: Vec<u8>) -> Result<Self, Self::Error> {
+        let mut mid = String::new();
+        let mut mname = String::new();
+        let mut mcreator = String::new();
+        let data = {
+            let mut vd = vec![];
+            let mut r =BufReader::new(Cursor::new(data));
+            r.read_line(&mut mid);
+            r.read_line(&mut mname);
+            r.read_line(&mut mcreator);
+        
+            r.read_to_end(&mut vd);
+            vd
+    
+        };
+        let mut r = Cursor::new(data);
+        let last_ms : u32 = r.read_le().or(Err(()))?;
+        let note_count : u32 = r.read_le().or(Err(()))?;
+        let mut diff :u8 = r.read_le().or(Err(()))?;
+        let mut img_type : u8 = r.read_le().or(Err(()))?;
+        match img_type {
+            2 => {
+                let mut len : u64 = r.read_le().or(Err(()))?;
+                r.seek(SeekFrom::Current(len as i64)).or(Err(()))?;
+            }
+            _ => {}
+        }
+        let has_audio :u8 = r.read_le().or(Err(()))?;
+        if has_audio != 1 {
+            return Err(()); // no audio
+        }
+        let music_length : u64 = r.read_le().or(Err(()))?;
+        let mut music_d = vec![0;music_length as usize];
+        r.read_exact(&mut music_d).or(Err(()))?;
+        let mut notes : Vec<SSPM1Note> = Vec::with_capacity(note_count as usize);
+        for i in 0..note_count {
+            let time : u32 = r.read_le().or(Err(()))?;
+            let ntype : u8 = r.read_le().or(Err(()))?;
+            notes.push(match ntype {
+                1 => {
+                    let x : f32 = r.read_le().or(Err(()))?;
+                    let y : f32 = r.read_le().or(Err(()))?;
+                    SSPM1Note::Float(SSPM1NoteF {
+                        time,
+                        x,
+                        y,
+                    })
+                }
+                _=> {
+                    let x : u8 = r.read_le().or(Err(()))?;
+                    let y : u8 = r.read_le().or(Err(()))?;
+                    SSPM1Note::Int(SSPM1Note8 {
+                        time,
+                        x : x as f32,
+                        y : y as f32,
+                    })
+                }
+            });
+        }
+        notes.sort_by(|x,y| x.time().cmp(&y.time()));
+        
+        
+        Ok(Self {
+            map_data : notes,
+            music_data : music_d,
+            id : mid,
+            name : mname,
+            creator : mcreator,
+        })
+    }
+}
+impl Into<FluxMap> for SSPM1 {
+    fn into(self) -> FluxMap {
+        let mut normalise_notes = String::new();
+        normalise_notes.push('0');
+        for note in self.map_data {
+            normalise_notes.push(',');
+            match note {
+                SSPM1Note::Float(x) => {
+                    normalise_notes.push_str(&format!("{}|{}|{}",x.x,x.y,x.time));
+                }
+                SSPM1Note::Int(x) => {
+                    normalise_notes.push_str(&format!("{}|{}|{}",x.x,x.y,x.time));
+                }
+            }
+        };
+        FluxMap {
+            artist : "<unknown>".to_string(),
+            mapper : self.creator,
+            song_name : self.name,
+            music_data : self.music_data,
+            map_data : normalise_notes.as_bytes().to_vec(),
+        }
+
+
+
+    }
 }
--- map-downloader/src/main.rs
+++ map-downloader/src/main.rs
@@ -1,7 +1,7 @@
 use std::path::PathBuf;
 
 use futures::{future::join_all, stream::FuturesUnordered, StreamExt};
-use map_creator::FluxMap;
+use map_creator::{FluxMap, convert::sspmv1::SSPM1};
 use mapdata::{DbRoot, MapRoot};
 use reqwest::Client;
 use tokio::sync::Semaphore;
@@ -17,7 +17,7 @@ async fn main() {
     let download_folder = PathBuf::from("./flux-downloads");
     std::fs::create_dir_all(&download_folder).expect("unable to create downloads folder");
     let sema = Semaphore::new(16);
-    let mut downloads = FuturesUnordered::new();
+    let mut downloads = Vec::new();
     for map_data in db_root.values() {
         let id = map_data.get("id").expect("unable to get id");
         let download_file_to = download_folder.join(format!("{}.flux",id.as_str().unwrap()));
@@ -32,36 +32,32 @@ async fn main() {
         downloads.push(save(map_data,download_file_to, client.clone(), &sema))
         
     }
-    futures::join!(downloads.into_future());
+    let _ = join_all(downloads).await;
 }
 async fn save(map_data:MapRoot,path_to:PathBuf, client:Client, sema : &Semaphore) {
-    if let Ok((map_datad,music_data)) = download(map_data.id, map_data.music_format.unwrap_or("mp3".to_string()), client, sema).await {
-        let mut sspmw = path_to.clone();
-        sspmw.set_extension("sspm");
-        std::fs::write(sspmw, &map_datad).expect("aa");
+    if let Ok(map_datad) = download(map_data.id.clone(), client, sema).await {
+        // let mut sspmw = path_to.clone();
+        // sspmw.set_extension("sspm");
+        // std::fs::write(sspmw, &map_datad).expect("aa");
 
-        let convert_map = map_creator::convert::sspmv1::convert_sspm1(map_datad);
+        let map_datan = SSPM1::try_from(map_datad);
+        if let Ok(d) = map_datan {
+            println!("got map data, converting..");
+            // return;
+            
+            FluxMap::from(d.into()).save(path_to.clone());
+            println!("saved {:?}",&path_to)
+        } else {
+            println!("unable to parse map data for {}",map_data.id);
+        }
 
-        return;
-
-
-        FluxMap {
-            artist: "<UNKNOWN>".to_string(),
-            map_data: convert_map,
-            song_name: map_data.song,
-            mapper: map_data.author.join(",").to_string(),
-            music_data
-        }.save(path_to.clone());
-        println!("saved {:?}",&path_to)
     }
 }
-async fn download(id:String,format:String, client:Client, sema : &Semaphore) -> Result<(Vec<u8>,Vec<u8>),()> {
+async fn download(id:String, client:Client, sema : &Semaphore) -> Result<Vec<u8>,()> {
     let sem = sema.acquire().await;
     println!("downloading {id}");
     let map_file = format!("{DB}/maps/{id}.sspm");
-    let music_file = format!("{DB}/maps/{id}.{format}");
     let map_data = client.get(map_file).send().await.unwrap().bytes().await.unwrap().to_vec();
-    let music_data = client.get(music_file).send().await.unwrap().bytes().await.unwrap().to_vec();
     println!("done {id}");
-    Ok((map_data,music_data))
+    Ok(map_data)
 }
