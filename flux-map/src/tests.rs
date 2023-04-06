1b112ebe7ea4f3f6776c24140da10225455d5747
aixeria <44528100+pozm@users.noreply.github.com>
2023-04-06 08:55:36 +0100

    flux-map: alter test

--- flux-map/src/tests.rs
+++ flux-map/src/tests.rs
@@ -5,9 +5,12 @@ mod tests {
 
     #[test]
     fn parse_file() {
-        let path = std::path::PathBuf::from("./data/maps/ss_archive_akira_complex_-_ether_strike.flux");
+        println!("cwd: {:?}", std::env::current_dir().unwrap());
+        let path = std::path::PathBuf::from("../data/maps/ss_archive_akira_complex_-_ether_strike.flux");
         let map = FluxMap::open(path).unwrap();
-        println!("{:?}", map);
+        assert!(map.version == 1);
+        assert!(map.meta.len() == 3);
+        assert!(map.difficulties.len() == 1);
     }
 
 }
